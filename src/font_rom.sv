c8a9cd01f411bcdad9047f458dd67e86bee987da
denis <denismedvedev478@gmail.com>
2026-04-20 14:34:52 +0300

    added modified but obsolete font_rom

--- src/font_rom.sv
+++ src/font_rom.sv
@@ -1,33 +1,26 @@
-module font_rom (
-    input  logic        clk,
-    input  logic [4:0]  char,   // 0..11
-    input  logic [5:0]  x,      // 0..49
-    input  logic [5:0]  y,      // 0..39
-    output logic        pixel
+//// DEPRECATED MODULE ////
+module font_rom #(
+    parameter PIX_DEP = 8,
+    parameter GLYPH_W = 40,
+    parameter GLYPH_H = 50,
+    parameter SYM_NUM = 12,
+    
+    // dont change
+    parameter NUM_ADDR = GLYPH_H*SYM_NUM,  //600 //кол-во адресов
+    parameter ADDR_W   = $clog2(NUM_ADDR), //10
+    parameter WORD_W   = GLYPH_W*PIX_DEP   //320
+)(
+    input  logic             clk,
+    input  logic[ADDR_W-1:0] addr_i,
+    output logic[WORD_W-1:0] word_o
 );
-    localparam CHAR_W = 50;
-    localparam CHAR_H = 40;
-    localparam CHARS = 12;
-    localparam TOTAL_BITS = CHARS * CHAR_W * CHAR_H; // 24000
-    localparam TOTAL_BYTES = TOTAL_BITS / 8;         // 3000
-
-    (* rom_style = "block" *) logic [7:0] mem [0:TOTAL_BYTES-1];
+    (* rom_style = "block" *) logic [WORD_W-1:0] mem [0:NUM_ADDR-1];
 
     initial begin
         $readmemh("font.hex", mem);
     end
 
-    // вычисляем битовый адрес (0..23999)
-    logic [14:0] bit_addr;
-    assign bit_addr = (char * CHAR_H + y) * CHAR_W + x;
-
-    logic [7:0] byte_data;
-    logic [2:0] bit_pos;
-
     always_ff @(posedge clk) begin
-        byte_data <= mem[bit_addr[14:3]];  // номер байта (старшие 12 бит)
-        bit_pos   <= bit_addr[2:0];        // номер бита в байте (0..7)
-        // Поправка порядка битов: первый пиксель (x=0) в старшем бите
-        pixel <= byte_data[7 - bit_pos];
+        word_o <= mem[addr_i];
     end
 endmodule
